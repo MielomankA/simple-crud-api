ac6e4a0845e592a07c21e89d37987e29ca4d8569
MielomankA <mielomanka@mail.ru>
2025-11-12 00:39:08 +0300

    feat: change users methods

--- src/db/users.ts
+++ src/db/users.ts
@@ -2,47 +2,47 @@ import { User } from '../types/types.js';
 
 const users: User[] = [];
 
-export const getAllUsers = (): User[] => {
-  return users;
+export const getAllUsers = async (): Promise<User[]> => {
+  return new Promise((resolve) => {
+    resolve(users);
+  });
 };
 
 export const getUserById = async (id: string): Promise<User | null> => {
-  return new Promise((resolve) =>
-    setTimeout(() => {
-      const user = users.find((user) => user.id === id) ?? null;
-      resolve(user);
-    }, 20)
-  );
+  return new Promise((resolve) => {
+    const user = users.find((user) => user.id === id) ?? null;
+    resolve(user);
+  });
 };
 
-export const createUser = (user: User): void => {
-    users.push(user);
+export const createUser = async (user: User): Promise<void> => {
+    return new Promise((resolve) => {
+      users.push(user);
+      resolve();
+    });
 };
 
 export const updateUser = async ({ id, username, age, hobbies }: User): Promise<User | null> => {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      const index = users.findIndex((user) => user.id === id);
-      if (index === -1) {
-        resolve(null);
-      } else {
-        users[index] = { id, username, age, hobbies };
-        resolve(users[index]);
-      }
-    }, 20);
+    const index = users.findIndex((user) => user.id === id);
+
+    if (index === -1) {
+      resolve(null);
+    } else {
+      users[index] = { id, username, age, hobbies };
+      resolve(users[index]);
+    }
   });
 };
 
 export const deleteUser = async (id: string): Promise<User | null> => {
   return new Promise((resolve) => {
-    setTimeout(() => {
-      const index = users.findIndex((user) => user.id === id);
-      if (index === -1) {
-        resolve(null);
-      } else {
-        users.splice(index, 1);
-        resolve(users[index]);
-      }
-    }, 20);
+    const index = users.findIndex((user) => user.id === id);
+    if (index === -1) {
+      resolve(null);
+    } else {
+      users.splice(index, 1);
+      resolve(users[index]);
+    }
   });
 };
